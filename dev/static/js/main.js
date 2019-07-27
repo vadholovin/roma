7a1381a589d99718431d31550725dbadf8438407
Vadym Holovin <holyvad@gmail.com>
2019-07-27 16:55:58 +0300

    Made desktop layout

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,37 @@
 $(document).ready(function () {
     svg4everybody({});
+
+
+
+
+    /**
+   * Tabs
+   */
+  let clickTabs = function () { 
+    let tabBtn = $('.js-tabs-nav > *'),
+        tabContent = $('.tab'),
+        tabName;
+    
+    tabBtn.click(function (e) {
+      e.preventDefault();
+      tabBtn.removeClass('is-active');
+      $(this).addClass('is-active');
+      tabName = $(this).data('tab-name');
+      selectTab(tabName);
+    });
+
+    function selectTab(tabName) {
+      tabContent.each(function() {
+        if ($(this).hasClass(tabName)) {
+          $(this).addClass('is-active')
+        } else {
+          $(this).removeClass('is-active');
+        }
+      });
+    }
+  };
+
+
+
+  clickTabs();
 });
